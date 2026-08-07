3fcd793e2e4aa2e20b61fabc858e6df7db8bb0e5
Mohammed Irfan <irf2k2@gmail.com>
2026-08-07 14:00:59 +0530

    file updates

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -30,7 +30,7 @@ class DefaultFirebaseOptions {
       case TargetPlatform.macOS:
         return macos;
       default:
-        throw UnsupportedException(
+        throw UnsupportedError(
           'DefaultFirebaseOptions are not configured for platform '
           '$defaultTargetPlatform.',
         );
